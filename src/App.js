15d8f5ca80d84361a4793683e77a933557acb4e7
Jake Morris <Jakeamorr@gmail.com>
2022-01-12 14:17:30 -0500

    remove about page, add link to resume

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { ThemeContext } from "./contexts/themeContext";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Projects from "./pages/Projects";
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
         <Route path="projects" element={<Projects />} />
       </Routes>
     </div>
