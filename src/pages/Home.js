582707f955418299c11f65bf82d054b877d7a235
Jake Morris <Jakeamorr@gmail.com>
2022-01-12 15:35:14 -0500

    add styling for light/dark mode profile pic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
+import { useContext } from "react";
 import image from "../images/profile_pic.jpg";
 import styleSheet from "./Home.module.css";
+import { ThemeContext } from "../contexts/themeContext";
 
 export default function Home() {
+  const theme = useContext(ThemeContext);
+  const getImageStyle = () => {
+    return theme === "light"
+      ? styleSheet.profilePic__light
+      : styleSheet.profilePic__dark;
+  };
+
   return (
     <div className={styleSheet.container}>
       <div className={styleSheet.home}>
-        <img src={image} alt="Me" className={styleSheet.profilePic} />
+        <img src={image} alt="Me" className={getImageStyle()} />
         <div className={styleSheet.aboutMe}>
           <p>Hey, my name is Jake</p>
           <p>
