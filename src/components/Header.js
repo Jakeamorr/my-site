802b6ee40665039531fa7213b088ddd07cc1c34e
Jake Morris <Jakeamorr@gmail.com>
2022-01-12 13:26:40 -0500

    add styling to the home page as well as some content

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,17 @@ export default function Header() {
   const { toggleTheme } = useContext(ThemeContext);
   return (
     <div className={styleSheet.header}>
+      <span></span>
       <nav className={styleSheet.navbar}>
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
-        <Link to="projects">Projects</Link>
+        <Link to="/" className={styleSheet.navbarLink}>
+          Home
+        </Link>
+        <Link to="about" className={styleSheet.navbarLink}>
+          About
+        </Link>
+        <Link to="projects" className={styleSheet.navbarLink}>
+          Projects
+        </Link>
       </nav>
       <button onClick={toggleTheme}>Toggle</button>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,27 @@
+import image from "../images/profile_pic.jpg";
+import styleSheet from "./Home.module.css";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className={styleSheet.container}>
+      <div className={styleSheet.home}>
+        <img src={image} alt="Me" className={styleSheet.profilePic} />
+        <div className={styleSheet.aboutMe}>
+          <p>Hey, my name is Jake</p>
+          <p>
+            I am currently exploring the vast world of bits and bytes in search
+            of a more meaningful path in programming. I work predominately as a
+            front-end developer but I have interest and experience in other
+            areas such as: machine learning, hardware, and IOT.
+          </p>
+          <p>
+            I am currently exploring the vast world of bits and bytes in search
+            of a more meaningful path in programming. I work predominately as a
+            front-end developer but I have interest and experience in other
+            areas such as: machine learning, hardware, and IOT.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
