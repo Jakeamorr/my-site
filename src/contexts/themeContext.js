77dc98251fc32f7b17b848b198a638f3ba057b05
Jake Morris <Jakeamorr@gmail.com>
2022-01-12 10:09:10 -0500

    add theme context

---
+++ src/contexts/themeContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+function ThemeContextProvider(props) {
+  const [theme, setTheme] = useState("dark");
+
+  function toggleTheme() {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+}
+
+export { ThemeContextProvider, ThemeContext };
