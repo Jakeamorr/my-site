4c3b17dde5866c41b2f24801e429573546214817
DO <jakeamorr@gmail.com>
2022-01-20 02:56:58 +0000

    switch routers for one that works with a static site

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -9,9 +9,9 @@ import { ThemeContextProvider } from "./contexts/themeContext";
 ReactDOM.render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
